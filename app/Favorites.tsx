b36083a64f134b8c7947329ece30b7c04ad791a4
FonzyLima <alfonsogvlima@gmail.com>
2024-05-06 17:43:08 +0800

    faves almost

--- app/Favorites.tsx
+++ app/Favorites.tsx
@@ -1,13 +1,63 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView ,StyleSheet, TouchableOpacity} from 'react-native'
 import React from 'react'
 import Footer from './components/shared/Footer'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Recipe from './components/shared/Recipe';
+import { useState,useEffect } from 'react';
+interface Recipe {
+  label: string;
+  healthLabels: string[];
+  ingredientLines: string[];
+  calories: number;
+  image: string;
+  source: string;
+  dishType: string;
+  
+    protein: number;
+    fat: number;
+    carbs: number;
+  
+}
 const Page = () => {
+  const [faves,setFaves] = useState<Recipe[]>([])
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('my-fave');
+      // await AsyncStorage.clear()
+      setFaves(JSON.parse(value))
+      console.log("herse", faves)
+    } catch (e) {
+      // error reading value
+      console.log(e)
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Here are your favorite Recipes</Text>
+      <ScrollView>
+        {/* <Recipe name={faves[0].label} calories={faves.calories} source={faves.source} image={faves.image} ingredientList={faves.ingredientLines} protein={faves.protein} carbs={faves.carbs} fat={faves.fat}/>
+         */}
+         {/* {faves ? faves.map((fave,i)=><Text key={i}>{fave[0].label}</Text>):null} */}
+      
+      </ScrollView>
       <Footer/>
     </View>
   )
 }
+const styles= StyleSheet.create({
+  container:{
+    padding:16,
+    height: "100%",
+    backgroundColor: "#FFF",
+  },
+  header:{
+    textAlign:"center",
+    fontWeight:"700",
+    fontSize:28
+  }
+})
 
 export default Page
--- app/[recipe].tsx
+++ app/[recipe].tsx
@@ -8,13 +8,66 @@ import {
 } from "react-native";
 import React from "react";
 import Footer from "./components/shared/Footer";
-import { useLocalSearchParams,router } from "expo-router";
+import { useLocalSearchParams, router } from "expo-router";
 import Icon from "react-native-vector-icons/FontAwesome6";
 import Icons from "react-native-vector-icons/MaterialCommunityIcons";
-import Heart from "react-native-vector-icons/AntDesign"
-import { useState,useEffect } from "react";
+import Heart from "react-native-vector-icons/AntDesign";
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+interface Recipe {
+  label: string;
+  healthLabels: string[];
+  ingredientLines: string[];
+  calories: number;
+  image: string;
+  source: string;
+  dishType: string;
+  
+  protein: number;
+  fat: number;
+  carbs: number;
+  
+}
 const Page = () => {
-  const [favorite,setFavorite] = useState(false)
+  const [faves,setFaves] = useState<Recipe[]>([])
+  const [favorite, setFavorite] = useState(false);
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('my-fave');
+      if(value!==null){
+        setFaves(value)
+      }
+      
+      console.log("hey",faves)
+    } catch (e) {
+      // error reading value
+    }
+  };
+  const storeData = async (value: any) => {
+    try {
+      
+      console.log(faves)
+      await AsyncStorage.setItem('my-fave', JSON.stringify(value));
+      console.log("success");
+    } catch (e) {
+      // saving error
+      console.log(e);
+    }
+  };
+  const addFavorite = (newRecipe: Recipe) => {
+    setFavorite(!favorite);
+    const updatedFaves = [...faves, newRecipe];
+    setFaves(updatedFaves);
+    storeData(updatedFaves);
+  };
+
+  
+  
+  useEffect(() => {
+    getData();
+  }, []);
+  
   const {
     recipe,
     name,
@@ -26,11 +79,12 @@ const Page = () => {
     fat,
     calories,
   } = useLocalSearchParams();
-  const parsedIngredientList = ingredientList?JSON.parse(ingredientList):[];
-  
+  const parsedIngredientList = ingredientList ? JSON.parse(ingredientList) : [];
   return (
     <View style={styles.container}>
-      <View><Image style={styles.image} source={{ uri: image as any}} /></View>
+      <View>
+        <Image style={styles.image} source={{ uri: image as any }} />
+      </View>
       <ScrollView
         contentContainerStyle={{
           paddingVertical: 16,
@@ -39,18 +93,35 @@ const Page = () => {
         <View style={styles.headerContainer}>
           <View style={styles.titleContainer}>
             <Text style={styles.nameText}>{name}</Text>
-            <TouchableOpacity onPress={()=>setFavorite(!favorite)}>
+            <TouchableOpacity
+              onPress={() =>
+                addFavorite({
+                  label: name,
+                  image: image,
+                  source: source,
+                  protein: protein,
+                  carbs: carbs,
+                  fat: fat,
+                  calories: calories,
+                })
+              }
+            >
+              {favorite ? (
+                <Heart name="heart" size={30} color="red" />
+              ) : (
+                <Heart name="hearto" size={30} color="red" />
+              )}
+            </TouchableOpacity>
+            {/* <TouchableOpacity onPress={()=>storeData({label:"helloss"})}>
               {favorite ? <Heart name="heart" size={30} color="red" />: <Heart name="hearto" size={30} color="red" /> }
               
-            </TouchableOpacity>
+            </TouchableOpacity> */}
           </View>
           <Text style={styles.grayText}>{source}</Text>
           <View style={styles.nutrientsContainer}>
             <View style={styles.nutrientsRow}>
               <View style={styles.nutrientBox}>
-                <View
-                  style={styles.iconBox}
-                >
+                <View style={styles.iconBox}>
                   <Icon name="fire" size={18} color="#000" />
                 </View>
                 <Text style={{ fontSize: 16 }}>
@@ -58,21 +129,21 @@ const Page = () => {
                 </Text>
               </View>
               <View style={styles.nutrientBox}>
-                <View
-                  style={styles.iconBox}
-                >
+                <View style={styles.iconBox}>
                   <Icons name="food-steak" size={20} color="#000" />
                 </View>
-                <Text style={{ fontSize: 16 ,flex:1}} numberOfLines={1} ellipsizeMode="tail">
+                <Text
+                  style={{ fontSize: 16, flex: 1 }}
+                  numberOfLines={1}
+                  ellipsizeMode="tail"
+                >
                   {Math.round(Number(protein))}g protein
                 </Text>
               </View>
             </View>
             <View style={styles.nutrientsRow}>
               <View style={styles.nutrientBox}>
-                <View
-                  style={styles.iconBox}
-                >
+                <View style={styles.iconBox}>
                   <Icon name="plate-wheat" size={18} color="#000" />
                 </View>
                 <Text style={{ fontSize: 16 }} numberOfLines={1}>
@@ -80,12 +151,14 @@ const Page = () => {
                 </Text>
               </View>
               <View style={styles.nutrientBox}>
-                <View
-                 style={styles.iconBox}
-                >
+                <View style={styles.iconBox}>
                   <Icon name="pizza-slice" size={18} color="#000" />
                 </View>
-                <Text style={{ fontSize: 16,flex:1 }} numberOfLines={1} ellipsizeMode="tail">
+                <Text
+                  style={{ fontSize: 16, flex: 1 }}
+                  numberOfLines={1}
+                  ellipsizeMode="tail"
+                >
                   {Math.round(Number(fat))}g fat
                 </Text>
               </View>
@@ -113,8 +186,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFF",
   },
   image: {
-    width:"100%",
-    height:210,
+    width: "100%",
+    height: 210,
     resizeMode: "cover",
     borderTopLeftRadius: 12, // Top-left border radius
     borderTopRightRadius: 12, // Top-right border radius
@@ -146,7 +219,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   nutrientsContainer: {
-    marginTop:20,
+    marginTop: 20,
     gap: 6,
   },
   nutrientsRow: {
